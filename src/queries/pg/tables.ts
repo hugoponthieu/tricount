d7870676b85db44afb51d5054b80df1665f53a52
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2023-12-02 15:56:49 +0100

    adding user api to do add group

--- src/queries/pg/tables.ts
+++ src/queries/pg/tables.ts
@@ -1,3 +1,5 @@
+const { Pool } = require("../queries_utils");
+
 
 const poolTables = new Pool({
     user: 'test',
@@ -10,8 +12,29 @@ const poolTables = new Pool({
 const drop =
     "DROP TABLE IF EXISTS remboursements; DROP TABLE IF EXISTS depenses; DROP TABLE IF EXISTS users;";
 const createUsers =
-    "CREATE TABLE users (email varchar(50) PRIMARY KEY NOT NULL UNIQUE,nom varchar(30),prenom varchar(30),pwd varchar(200));"
+    "CREATE TABLE users (email varchar(50) PRIMARY KEY NOT NULL UNIQUE,nom varchar(30),prenom varchar(30),pseudonyme varchar(30),pwd varchar(200));"
 const createDepenses =
     "CREATE TABLE depenses (id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL UNIQUE,montant float NOT NULL,utilisateur varchar(50) NOT NULL,date DATE,FOREIGN KEY (utilisateur) REFERENCES users(email));"
 const createRemboursements =
     "CREATE TABLE remboursements (iddepense int ,utilisateur varchar(50),part float,FOREIGN KEY (utilisateur) REFERENCES users(email),FOREIGN KEY (iddepense) REFERENCES depenses(id),PRIMARY KEY (iddepense,utilisateur));"
+
+function createTables() {
+    poolTables.query(drop, (error, results) => {
+        if (error) { throw error }
+        console.log('Tables supprimées')
+    })
+    poolTables.query(createUsers, (error, results) => {
+        if (error) { throw error }
+        console.log('Db for user created')
+    })
+    poolTables.query(createDepenses, (error, results) => {
+        if (error) { throw error }
+        console.log('Db for depenses created')
+    })
+    poolTables.query(createRemboursements, (error, results) => {
+        if (error) { throw error }
+        console.log('Db for remboursements created')
+    })
+}
+
+export { createTables };
--- src/queries/user_queries.ts
+++ src/queries/user_queries.ts
@@ -1,15 +1,44 @@
 import { bodyParser, pool, express } from './queries_utils';
+import { hash } from 'bcrypt';
 
-const userRouteur = express.Router();
+const userRouter = express.Router();
 
-userRouteur.user(bodyParser.json());
+userRouter.user(bodyParser.json());
 
-userRouteur.get('/', (request, response) => {
-    pool.query('SELECT * FROM depense;', (error, results) => {
+userRouter.get('/', (request, response) => {
+    pool.query('SELECT email,nom,prenom,pseudonyme FROM users;', (error, results) => {
         if (error) {
             throw error
         }
         response.status(200).json(results.rows)
+    })
+})
+
+userRouter.post('/', (request, response) => {
+    const { email, nom, prenom, pwd } = request.body;
+    hash(pwd, 10).then(pool.query('insert into users values ($1,$2,$3,$4)', [email, nom, prenom, pwd], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    }))
+})
+
+userRouter.delete('/:email', (request, response) => {
+    pool.query('delete from depense where "email" =$1', [request.params.email], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.row)
+    })
+})
+
+userRouter.get('/:email', (request, response) => {
+    pool.query('select nom,prenom,pseudonyme from user where "email"=$1', [request.params.email], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.row)
     })
 })
 
